71ddf16bd7e39283456f74516302ae2a6431ce87
Maxim Egorov <abd.andrew@gmail.com>
2024-02-29 10:05:16 +0500

    Add clang-format

--- src/lang/Char.h
+++ src/lang/Char.h
@@ -1,15 +1,11 @@
 #ifndef LANG_CHAR_H_
 #define LANG_CHAR_H_
 
-
 #include <cstdint>
 
+namespace lang {
+using Char = uint8_t;
+using Char32 = uint32_t;
+}  // namespace lang
 
-namespace lang
-{
-	using Char = uint8_t;
-	using Char32 = uint32_t;
-}
-
-
-#endif // LANG_CHAR_H_
+#endif  // LANG_CHAR_H_
